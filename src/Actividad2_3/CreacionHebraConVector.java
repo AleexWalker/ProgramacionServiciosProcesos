b32d74f697d7b65d58d03c0aadcfefff3bbd18b9
alu10194039 <aluurs4039@ieselcaminas.org>
2021-10-08 17:07:13 +0200

    Final

--- src/Actividad2_3/CreacionHebraConVector.java
+++ src/Actividad2_3/CreacionHebraConVector.java
@@ -1,17 +1,40 @@
 package Actividad2_3;
 
 public class CreacionHebraConVector {
-    public static void main(String[] args) {
-        int [] vector = new int[5];
+
+    public static void main ( String args[] ) {
+        int numHebras = 5;
+
+        numHebra [] v = new numHebra [10];
+
+        for ( int i = 0; i < numHebras; i++ ) {
+            v[i] = new numHebra(i);
+        }
+
+        for ( int i = 0; i < numHebras; i++ ) {
+            v[i].start();
+        }
+
+        for ( int i = 0; i < numHebras; i++ ) {
+            try {
+                v[i].join();
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            };
+
+        }
+        System.out.println ( "Todas las hebras han acabado");
     }
 }
-class Tarea1 extends Thread{
-    int[] idHebra;
-    public Tarea1(int[] idHebra) {
+
+class numHebra extends Thread {
+    int idHebra;
+    public numHebra ( int idHebra ){
         this.idHebra = idHebra;
     }
-    public void run(){
-        //if (idHebra.equals("1"))
 
+    public void run () {
+        System.out.println ( "Hola soy la hebra: " + idHebra );
     }
 }
